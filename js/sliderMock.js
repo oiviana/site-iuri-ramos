3ddadf54ca4f964b71e474324e243b97035f7018
Lucas Viana <vianalucas80@gmail.com>
2024-12-24 16:38:54 -0300

    feat: slider title

--- js/sliderMock.js
+++ js/sliderMock.js
@@ -1,11 +1,11 @@
 export const slider1 = [
-    { id: 1, imgsrc: "./img/slider-1/Auto-escola-=-icone.png", imgtitle: "Slide 1" },
-    { id: 2, imgsrc: "./img/slider-1/Capas-corporativa-=-icone.png", imgtitle: "Slide 2" },
-    { id: 3, imgsrc: "./img/slider-1/Cardapio-=-icone.png", imgtitle: "Slide 3" },
-    { id: 4, imgsrc: "./img/slider-1/Educação-=-icone.png", imgtitle: "Slide 4" },
-    { id: 5, imgsrc: "./img/slider-1/Facebook-=-icone.png", imgtitle: "Slide 5" },
-    { id: 6, imgsrc: "./img/slider-1/Futebol-=-icone.png", imgtitle: "Slide 6" },
-    { id: 7, imgsrc: "./img/slider-1/Segurança-=-icone.png", imgtitle: "Slide 7" },
-    { id: 8, imgsrc: "./img/slider-1/SOCIAL-MEDIA-=-icone.png", imgtitle: "Slide 8" },
+    { id: 1, imgsrc: "./img/slider-1/Auto-escola-=-icone.png", imgtitle: "Slide 1", redirect: "#" },
+    { id: 2, imgsrc: "./img/slider-1/Capas-corporativa-=-icone.png", imgtitle: "Slide 2", redirect: "#"},
+    { id: 3, imgsrc: "./img/slider-1/Cardapio-=-icone.png", imgtitle: "Slide 3", redirect: "#" },
+    { id: 4, imgsrc: "./img/slider-1/Educação-=-icone.png", imgtitle: "Slide 4", redirect: "#" },
+    { id: 5, imgsrc: "./img/slider-1/Facebook-=-icone.png", imgtitle: "Slide 5", redirect: "#"},
+    { id: 6, imgsrc: "./img/slider-1/Futebol-=-icone.png", imgtitle: "Slide 6", redirect: "#" },
+    { id: 7, imgsrc: "./img/slider-1/Segurança-=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 8, imgsrc: "./img/slider-1/SOCIAL-MEDIA-=-icone.png", imgtitle: "Slide 8", redirect: "#" },
   ];
   
