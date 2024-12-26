8c6caf3d280d6a83c887db3ff40c70fda9aed1c7
Lucas Viana <vianalucas80@gmail.com>
2024-12-26 13:28:49 -0300

    test: slider 2

--- js/sliderMock.js
+++ js/sliderMock.js
@@ -9,3 +9,18 @@ export const slider1 = [
     { id: 8, imgsrc: "./img/slider-1/SOCIAL-MEDIA-=-icone.png", imgtitle: "Slide 8", redirect: "#" },
   ];
   
+  export const slider2 = [
+    { id: 1, imgsrc: "./img/slider-2/BELEZA-=-icone.png", imgtitle: "Slide 1", redirect: "#" },
+    { id: 2, imgsrc: "./img/slider-2/Capa---GRÁFICA.png", imgtitle: "Slide 2", redirect: "#"},
+    { id: 3, imgsrc: "./img/slider-2/CROSSFIT-=-icone.png", imgtitle: "Slide 3", redirect: "#" },
+    { id: 4, imgsrc: "./img/slider-2/Dia-dos-pais-=-icone.png", imgtitle: "Slide 4", redirect: "#" },
+    { id: 5, imgsrc: "./img/slider-2/Energia-solar-=-icone.png", imgtitle: "Slide 5", redirect: "#"},
+    { id: 6, imgsrc: "./img/slider-2/ESTETICA-AUTOMOTIVA--=-icone--oficial.png", imgtitle: "Slide 6", redirect: "#" },
+    { id: 7, imgsrc: "./img/slider-2/Evento-=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 8, imgsrc: "./img/slider-2/GAME--=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 9, imgsrc: "./img/slider-2/Igreja--=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 10, imgsrc: "./img/slider-2/IPTV--=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 11, imgsrc: "./img/slider-2/Moda-=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 12, imgsrc: "./img/slider-2/PREFEITURA-=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 13, imgsrc: "./img/slider-2/SUPERMERCADO--=-icone.png", imgtitle: "Slide 7", redirect: "#" }
+  ];
