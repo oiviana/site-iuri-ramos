4e93395105fea089e174289b435ba6c57af8177c
Lucas Viana <vianalucas80@gmail.com>
2025-01-06 13:51:14 -0300

    feat: last items

--- js/sliderMock.js
+++ js/sliderMock.js
@@ -1,7 +1,7 @@
 export const slider1 = [
     { id: 1, imgsrc: "./img/slider-1/Auto-escola-=-icone.png", imgtitle: "Slide 1", redirect: "https://drive.google.com/drive/folders/1AwPPaZ2vaET50GV_SD2XxKy1xSi_S63I" },
     { id: 2, imgsrc: "./img/slider-1/Capas-corporativa-=-icone.png", imgtitle: "Slide 2", redirect: "https://drive.google.com/drive/folders/1vDoBpz0zpqM6jPRrnLyMIQi4cBkoUjJ2"},
-    { id: 3, imgsrc: "./img/slider-1/Cardapio-=-icone.png", imgtitle: "Slide 3", redirect: "#" },
+    { id: 3, imgsrc: "./img/slider-1/Cardapio-=-icone.png", imgtitle: "Slide 3", redirect: "https://drive.google.com/drive/folders/1HqPQP0l4NlPOdMiJVocIDXI2XjQ3bdGP" },
     { id: 4, imgsrc: "./img/slider-1/Educação-=-icone.png", imgtitle: "Slide 4", redirect: "https://drive.google.com/drive/folders/1FS2YN3nwJGknswG-B4iEgW6fy7oIMlkT" },
     { id: 5, imgsrc: "./img/slider-1/Facebook-=-icone.png", imgtitle: "Slide 5", redirect: "https://drive.google.com/drive/folders/1rIwIhSngwoC-4Axe7mO4-EzYsele-vU9"},
     { id: 6, imgsrc: "./img/slider-1/Futebol-=-icone.png", imgtitle: "Slide 6", redirect: "https://drive.google.com/drive/folders/17v0CDwtuV4obu3xVg80kEMRkjWapD8ua" },
@@ -11,13 +11,13 @@ export const slider1 = [
   
   export const slider2 = [
     { id: 1, imgsrc: "./img/slider-2/BELEZA-=-icone.png", imgtitle: "Slide 1", redirect: "https://drive.google.com/drive/folders/1GfFuMrC4BOXWp-6neWDS70r6CC8JbgsZ" },
-    { id: 2, imgsrc: "./img/slider-2/Capa---GRÁFICA.png", imgtitle: "Slide 2", redirect: "#"},
+    { id: 2, imgsrc: "./img/slider-2/Capa---GRÁFICA.png", imgtitle: "Slide 2", redirect: "https://drive.google.com/drive/folders/1zyiMt0-2FHha_e5xII7vESi-9ELtXwWd"},
     { id: 3, imgsrc: "./img/slider-2/CROSSFIT-=-icone.png", imgtitle: "Slide 3", redirect: "https://drive.google.com/drive/folders/1SEFXTX4drLKOkTH-KnjSOWuU2YFg8n3C" },
     { id: 4, imgsrc: "./img/slider-2/Dia-dos-pais-=-icone.png", imgtitle: "Slide 4", redirect: "https://drive.google.com/drive/folders/18d8SdicH7OFNOIBjvZ_0_WQtWNG-LsXS" },
-    { id: 5, imgsrc: "./img/slider-2/Energia-solar-=-icone.png", imgtitle: "Slide 5", redirect: "#"},
-    { id: 6, imgsrc: "./img/slider-2/ESTETICA-AUTOMOTIVA--=-icone--oficial.png", imgtitle: "Slide 6", redirect: "#" },
+    { id: 5, imgsrc: "./img/slider-2/Energia-solar-=-icone.png", imgtitle: "Slide 5", redirect: "https://drive.google.com/drive/folders/1UzG_PkyVfT_bIOa3lC4efazjZAupuPll"},
+    { id: 6, imgsrc: "./img/slider-2/ESTETICA-AUTOMOTIVA--=-icone--oficial.png", imgtitle: "Slide 6", redirect: "https://drive.google.com/drive/folders/1FNQLH0l2PknRc9Bt3mf_zDpkFEc-XDbD" },
     { id: 7, imgsrc: "./img/slider-2/Evento-=-icone.png", imgtitle: "Slide 7", redirect: "https://drive.google.com/drive/folders/1AIw6osJdmJaFuwZUMuK6lAbHl880c588" },
-    { id: 8, imgsrc: "./img/slider-2/GAME--=-icone.png", imgtitle: "Slide 7", redirect: "#" },
+    { id: 8, imgsrc: "./img/slider-2/GAME--=-icone.png", imgtitle: "Slide 7", redirect: "https://drive.google.com/drive/folders/14Hog2vBI0WIEAC5cFob_fMddfA83F31i" },
     { id: 9, imgsrc: "./img/slider-2/Igreja--=-icone.png", imgtitle: "Slide 7", redirect: "https://drive.google.com/drive/folders/1hPgo0P_Hc8BsyPLSZg3OK3I56iOKk6jW" },
     { id: 10, imgsrc: "./img/slider-2/IPTV--=-icone.png", imgtitle: "Slide 7", redirect: "https://drive.google.com/drive/folders/1ujHjtV64A0QhBFqWyrIByNw3IGEGLe4e" },
     { id: 11, imgsrc: "./img/slider-2/Moda-=-icone.png", imgtitle: "Slide 7", redirect: "https://drive.google.com/drive/folders/1aRn3BKoi-7_E-vPrMXpiePgiEW1TT8-i" },
